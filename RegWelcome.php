7b8635b3a1222591584342af7dab58bf0a987f54
Md Humayun Rashid <92227819+hrs121@users.noreply.github.com>
2024-01-11 11:35:02 +0600

    first commit

---
+++ RegWelcome.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration complete</title>
+</head>
+<body>
+    <h1>You have successfully Registered.</h1>
+    <p><a href="./login.php">Click here to go to Login Page.</a></p>
+</body>
+</html>
